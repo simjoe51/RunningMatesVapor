6a2f369b5540824d00decc02444e388b30d2a6f7
Joseph Simeone <simjoe51@outlook.com>
2021-02-13 18:47:09 -0500

    MacBook 2/13/2021

--- Sources/App/Models/Letter.swift
+++ Sources/App/Models/Letter.swift
@@ -22,9 +22,21 @@ final class Letter: Model, Content {
     var senderID: String
     @Field(key: "letterData")
     var letterData: String
+    @Field(key: "beenOpened")
+    var beenOpened: Bool
     
     //empty initializer
     init() {}
+    
+    //do I really need an extra structure to get data out or can I just use the class itself? I shouldn't need to access one part without another
+    struct postLetter: Content {
+        var recipientID: String
+        var senderID: String
+        var letterData
+    }
+    
+    //struct to get the ID of a letter out of the database and return to user on creation
+    
     
     //constructor for new letter with all parameters filled (Shouldn't be any situation where they all aren't being filled at the same time, unlike an account)
     init(id: UUID?, rID: String, sID: String, data: String) {
@@ -33,4 +45,18 @@ final class Letter: Model, Content {
         self.senderID = sID
         self.letterData = data
     }
+}
+
+struct LetterController {
+    //Create new letter
+    func newLetter(req: Request) throws -> EventLoopFuture<Letter> {
+        input = try req.content.decode(Letter)
+        
+        let id = UUID()
+        let letter = Letter(id: id, rID: input.rec, sID: <#T##String#>, data: <#T##String#>)
+    }
+    
+    //Called when the a user has read a letter. Use a webhook to update other client's screen when this happens and show an opened envelope
+    func openedLetter(req: Request) throws -> EventLoopFuture<Bool>
+    //update 'beenOpened to reflect'
 }
